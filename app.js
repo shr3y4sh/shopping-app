6a65acdc954f245e557207f853cffe9e01f8c3ca
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2025-03-03 14:05:33 +0530

    edit views, templates, basic ui stuff

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', 'views/templates');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
